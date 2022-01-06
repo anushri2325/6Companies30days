02333b98b2f1c432b2125cce8d624ac794827645
Anushri Jain <61342659+anushri2325@users.noreply.github.com>
2022-01-06 16:09:59 +0530

    Update 03_ipl2021.cpp

--- Amazon/03_ipl2021.cpp
+++ Amazon/03_ipl2021.cpp
@@ -1,7 +1,7 @@
 Problem Link : https://practice.geeksforgeeks.org/problems/deee0e8cf9910e7219f663c18d6d640ea0b87f87/1/
 
 
-// Absolutely same as question 2
+// Absolutely same as question 6
 *********************************************************************************************************
 
 class Solution {
