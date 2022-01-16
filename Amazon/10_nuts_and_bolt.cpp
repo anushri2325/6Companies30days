b689df959bc03f54c18f02a2b2f9836d844f43fe
Anushri Jain <61342659+anushri2325@users.noreply.github.com>
2022-01-16 15:24:56 +0530

    Create 10_nuts_and_bolt.cpp

---
+++ Amazon/10_nuts_and_bolt.cpp
@@ -0,0 +1,14 @@
+Problem Link : https://practice.geeksforgeeks.org/problems/nuts-and-bolts-problem0431/1
+
+*************************************************************************************************
+
+class Solution{
+public:	
+
+	void matchPairs(char nuts[], char bolts[], int n) {
+	    // code here
+	    sort(nuts,nuts+n);
+	    sort(bolts,bolts+n);
+	}
+
+};
